ff1b8ec5a3e11028fb6757dc270731dcdbcaa7d5
mei654654 <91170742+mei654654@users.noreply.github.com>
2022-02-24 17:41:39 +0800

    Add files via upload

---
+++ week01/week01-2.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+    ///int a;
+    ///printf("請輸入一個數字");
+    ///scanf("%d",&a);
+    ///printf("你讀到了 %d\n",a);
+    char c;
+    printf("請輸入一堆字: ");
+    scanf("%c",&c);
+    printf("你讀到了 %c\n",c);
+
+    return 0;
+}
